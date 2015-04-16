8cdab9dae96dc245d0ba5e6bbe14204405dc8e2c
Daniel Kugel <mail4dk@gmail.com>
2015-04-16 08:57:50 +0300

    Path separator changed for multiplatform support.

--- src/autoload.php
+++ src/autoload.php
@@ -1,8 +1,8 @@
 <?php
 //spl_autoload_extensions('.php');
 spl_autoload_register(function ($class) {
-    include_once 'src\\Table.php';
-	include_once 'src\\TableOne.php';
+    include_once 'src/Table.php';
+	include_once 'src/TableOne.php';
 });
 
 ?>
